3383dbffecf9f2662a1671ee92982a08e69128fc
Khristoforov Ilya <hristoforov.ilya@gmail.com>
2018-10-31 21:38:02 +0300

    Arrow functions, Destruction

--- es-6-7-8.php
+++ es-6-7-8.php
@@ -13,11 +13,11 @@
  **/
 	var pizza = true;
 	pizza = false;
-	console.log(pizza);
+	//console.log(pizza);
 	
 	const pizza2 = 2;
 	//pizza2 = 3; // Error
-	console.log(pizza2);
+	//console.log(pizza2);
 
 /**
  * LET
@@ -28,22 +28,22 @@
 		let topic = 5;
 		console.log('block', topic);
 	}
-	console.log('global', topic);
+	//console.log('global', topic);
 
 /**
  * `${}`
  * template for vars
  **/
-	console.log(`${pizza}, ${pizza2}, ${topic}`);
+	//console.log(`${pizza}, ${pizza2}, ${topic}`);
 
-	console.log(`
+	/*console.log(`
 		Hello, ${pizza}!
 
 		Whitespaces and formating remain untuched.
 		${pizza2}!
 
 		${topic}
-	`);
+	`);*/
 
 /**
  * DEFAULT PARAMETERS
@@ -66,8 +66,8 @@
 	function introduction( p = defaultPerson ){
 		console.log(`Hello! My name is ${p.name.first} ${p.name.last}. I\'m a ${p.profession}`);
 	}
-	introduction();
-	introduction( newPerson );
+	//introduction();
+	//introduction( newPerson );
 
 /**
  * ARROW FUNCTION
@@ -79,8 +79,49 @@
 	var arrow_func1 = firstName => console.log(`Arrow function test name is: ${firstName}`);
 	var arrow_func2 = (firstName,lastName) => console.log(`Arrow function test name is: ${firstName} ${lastName}`);
 	
-	arrow_func1(fname);
-	arrow_func2(fname, lname);
+	//arrow_func1(fname);
+	//arrow_func2(fname, lname);
+
+/**
+ * ARROW FUNCTION
+ * var function_name = x => [function body]
+ **/
+	var arrow_func3 = (firstName,lastName) => {
+		if(!firstName){
+			throw new Error('firstName is required');
+		}
+		
+		if(!lastName){
+			throw new Error('lastName is required');
+		}
+		console.log(`Arrow function test name is: ${firstName} ${lastName}`);
+	}
+	//arrow_func3('xyz');
+
+/**
+ * DESCTRUCTION
+ **/
+	var x = {
+		x1: "Alex",
+		x2: "Ann"
+	}
+	var {x2} = x;
+	console.log(x2);
+	
+	lordify = (z) => {
+		console.log(`${z.x1} is a lord`);
+	}
+	lordify(x);		// Alex is a second lord
+	
+	lordify2 = ({x1}) => {
+		console.log(`${x1} is a second lord`);
+	}
+	lordify2(x);	// Alex is a second lord
+
+	// destruction in array
+	var arr = ["x","y","z"];
+	var [,,d] = arr;
+	console.log(d);	// z
 </script>
 
 </body>
